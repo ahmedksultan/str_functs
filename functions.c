03ccf8e631b638451b4d078d3e9377a95c3aad84
asultan00 <asultan00@stuy.edu>
2019-09-26 11:48:07 -0400

    edited

---
+++ functions.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int strlen(char *s) {
+    return 0;
+}
+
+char *strcpy(char *dest, char *source) {
+    char s;
+    return s;
+}
+
+char *strcat(char *dest, char *source) {
+    char s;
+    return s;
+}
+
+int strcmp(char *s1, char *s2) {
+    return 0;
+}
+
+char *strchr(char *s, char c) {
+    char s;
+    return s;
+}
